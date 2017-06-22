251b63b3cdee96439db629e309cca96080459329
Jesse DiGregorio <jesse.digregorio@live.com>
2017-06-21 20:21:30 -0700

    Work for TextView practice. Not sure what's up, but now it won't deploy to the emulator.

--- MyApplication/app/src/main/java/com/example/jesse/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/jesse/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        /*
+        final TextView textViewToChange = (TextView) findViewById(R.id.practiceTextView);
+        textViewToChange.setText("HOWDY!");
+        */
     }
 
     @Override
